ac624c1fca7763b74153f5b484dc0aff24155dda
M Haris Nadeem <harisnadeemshk@gmail.com>
2023-10-29 21:51:00 +0500

    changes

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(passport.session());
 
 app.use(
 	cors({
-		origin: "https://demologin-weld.vercel.app/",
+		origin: "https://demologin-weld.vercel.app",
 		methods: "GET,POST,PUT,DELETE",
 		credentials: true,
 	})
